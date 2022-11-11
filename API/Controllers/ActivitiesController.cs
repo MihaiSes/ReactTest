d6fe308b7f47c48581b610a90964fe5626438ff0
Mihai Sescioreanu <mihais@roweb.ro>
2022-11-11 14:24:58 +0200

    -second commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(ILogger<BaseApiController> logger,
+                                    DataContext context ) 
+                    : base(logger)
+        {
+                _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+                return await _context.Activities.ToListAsync();
+        }
+
+         [HttpGet("{id}")]
+         public async Task<ActionResult<Activity>> GetActivity(Guid id)
+         {
+            return await _context.Activities.FindAsync(id);
+         }
+
+    }
+}
